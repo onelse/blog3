24b778afbc98e607a7057c553d493f3daeed514e
unknown <kjhhjk5051@syuin.ac.kr>
2019-03-06 22:06:39 +0900

    3th

--- homework/settings.py
+++ homework/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', '_do!apxvlg211g&(bn6yi@@jq+2^kz
 #DEBUG = True
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
